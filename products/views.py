0b1a6b8d947afa1662055bdaca61a07e4ea16767
nlenno1 <nick.lennon@hotmail.co.uk>
2022-02-06 18:00:30 +0000

    fix: correct typing error in calling is_superuser()

--- products/views.py
+++ products/views.py
@@ -83,9 +83,9 @@ def product_detail(request, product_id):
 @login_required
 def add_product(request):
     """ Add a product to the store"""
-    if not request.user.super_user:
+    if not request.user.is_superuser:
         messages.error(request, "Sorry, only store owners can do that!")
-        return redirect(revers('home'))
+        return redirect(reverse('home'))
     
     if request.method == "POST":
         form = ProductForm(request.POST, request.FILES)
@@ -109,9 +109,9 @@ def add_product(request):
 @login_required
 def edit_product(request, product_id):
     """ Edit a product in the store """
-    if not request.user.super_user:
+    if not request.user.is_superuser:
         messages.error(request, "Sorry, only store owners can do that!")
-        return redirect(revers('home'))
+        return redirect(reverse('home'))
     
     product = get_object_or_404(Product, pk=product_id)
     if request.method == "POST":
@@ -138,9 +138,9 @@ def edit_product(request, product_id):
 @login_required
 def delete_product(request, product_id):
     """ Delete a product from the store """
-    if not request.user.super_user:
+    if not request.user.is_superuser:
         messages.error(request, "Sorry, only store owners can do that!")
-        return redirect(revers('home'))
+        return redirect(reveres('home'))
     
     product = get_object_or_404(Product, pk=product_id)
     product.delete()
